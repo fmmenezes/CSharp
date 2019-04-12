ae087a9c214eaee1360596f6d4300a3aa6f2d4c3
Fernando Mendes de Menezes <fmmenezes@hotmail.com>
2019-04-11 22:26:38 -0300

    Criação do Programa

---
+++ Recapitulacao/Recapitulacao/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Recapitulacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com seu nome completo:");
+            Console.WriteLine("Quantos quartos tem na sua casa?");
+            Console.WriteLine("Entre com o preço de um produto:");
+            Console.WriteLine("Entre com seu ultimo nome, idade e atura (mesma linha):");
+        }
+    }
+}

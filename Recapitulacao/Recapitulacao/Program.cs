92a63f6a76d82ffb930c7b8bf415c492c3b5def5
Fernando Mendes de Menezes <fmmenezes@hotmail.com>
2019-04-11 22:50:10 -0300

    Inclusão de Funcionalidades

--- Recapitulacao/Recapitulacao/Program.cs
+++ Recapitulacao/Recapitulacao/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Recapitulacao
 {
@@ -6,10 +7,29 @@ namespace Recapitulacao
     {
         static void Main(string[] args)
         {
+            string nome;
+            int quartos;
+            double preco;
+            string[] linha;
+
             Console.WriteLine("Entre com seu nome completo:");
+            nome = Console.ReadLine();
+
             Console.WriteLine("Quantos quartos tem na sua casa?");
+            quartos = int.Parse(Console.ReadLine());
+
             Console.WriteLine("Entre com o preço de um produto:");
+            preco = double.Parse(Console.ReadLine());
+
             Console.WriteLine("Entre com seu ultimo nome, idade e atura (mesma linha):");
+            linha = Console.ReadLine().Split(' ');
+
+            Console.WriteLine(nome);
+            Console.WriteLine(quartos);
+            Console.WriteLine(preco.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(linha[0]);
+            Console.WriteLine(linha[1]);
+            Console.WriteLine(linha[2]);
         }
     }
 }

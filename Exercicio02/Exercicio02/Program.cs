0f2aecf08e8c5985800ae8f0cff0fdc5bb55593a
Fernando Mendes de Menezes <fmmenezes@hotmail.com>
2019-04-12 22:59:21 -0300

    Criação do Programa

---
+++ Exercicio02/Exercicio02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double PI = 3.14159;
+            double raio;
+            double resultado;
+
+            Console.WriteLine("Digite o valor:");
+            raio = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(raio);
+
+            resultado = PI * raio * raio;
+
+            Console.WriteLine(resultado.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}

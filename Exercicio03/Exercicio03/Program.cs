3c863a1afa15ff0c5fabd2ea52665542cda3731c
Fernando Mendes de Menezes <fmmenezes@hotmail.com>
2019-04-13 15:56:25 -0300

    Criação do Programa

---
+++ Exercicio03/Exercicio03/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exercicio03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor 1?");
+            Console.WriteLine("Digite o valor 2?");
+            Console.WriteLine("Digite o valor 3?");
+            Console.WriteLine("Digite o valor 4?");
+        }
+    }
+}

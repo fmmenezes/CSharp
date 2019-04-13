5f67f870a675c60c07c2ea89f8cf2619f8c1b8dd
Fernando Mendes de Menezes <fmmenezes@hotmail.com>
2019-04-13 16:24:30 -0300

    Finalização

--- Exercicio03/Exercicio03/Program.cs
+++ Exercicio03/Exercicio03/Program.cs
@@ -7,9 +7,17 @@ namespace Exercicio03
         static void Main(string[] args)
         {
             Console.WriteLine("Digite o valor 1?");
+            int valor1 = int.Parse(Console.ReadLine());
             Console.WriteLine("Digite o valor 2?");
+            int valor2 = int.Parse(Console.ReadLine());
             Console.WriteLine("Digite o valor 3?");
+            int valor3 = int.Parse(Console.ReadLine());
             Console.WriteLine("Digite o valor 4?");
+            int valor4 = int.Parse(Console.ReadLine());
+
+            int diferenca = (valor1 * valor2 - valor3 * valor4);
+
+            Console.WriteLine("DIFERENCA = " + diferenca);
         }
     }
 }

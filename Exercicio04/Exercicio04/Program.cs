6be83ad82370190ed58762d95e6addc34f8102d3
Fernando Mendes de Menezes <fmmenezes@hotmail.com>
2019-04-13 20:52:14 -0300

    Finalizado.

---
+++ Exercicio04/Exercicio04/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercicio04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Codigo do Funcionario: ");
+            int codigo = int.Parse(Console.ReadLine());
+            Console.WriteLine("Quantidade de Horas Trabalhadas: ");
+            int horas = int.Parse(Console.ReadLine());
+            Console.WriteLine("Valor Hora: ");
+            double valor = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("NUMBER: " + codigo);
+            double salario = horas * valor;
+            Console.WriteLine("SALARY: U$ " + salario.ToString("F2"));
+        }
+    }
+}

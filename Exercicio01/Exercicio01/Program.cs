ffcb459f9cceca09a15122e5349af9c31e321251
Fernando Mendes de Menezes <fmmenezes@hotmail.com>
2019-04-12 22:00:32 -0300

    Criação do programa

---
+++ Exercicio01/Exercicio01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor1;
+            int valor2;
+
+            Console.WriteLine("Informe o primeiro valor");
+            valor1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o segundo valor");
+            valor2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(valor1 + valor2);
+        }
+    }
+}

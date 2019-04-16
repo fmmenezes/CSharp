4c4467871e72335ed8717f8c89a9feb282d26ed2
Fernando Mendes de Menezes <fmmenezes@hotmail.com>
2019-04-16 20:57:10 -0300

    Ler um número inteiro e dizer se este número é par ou ímpar

---
+++ ExercicioCond02/ExercicioCond02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExercicioCond02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero inteiro:");
+            int numero = int.Parse(Console.ReadLine());
+            int resto = numero % 2;
+
+            if (resto == 0)
+            {
+                Console.WriteLine("PAR");
+            }
+            else
+            {
+                Console.WriteLine("IMPAR");
+            }
+        }
+    }
+}

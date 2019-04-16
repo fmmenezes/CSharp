33df7866e1a284a4899398268e9375c258bf1d0c
Fernando Mendes de Menezes <fmmenezes@hotmail.com>
2019-04-16 20:44:54 -0300

    Ler um número inteiro e depois dizer se este número é negativo ou não.

---
+++ ExercicioCond01/ExercicioCond01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExercicioCond01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número inteiro:");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero >= 0)
+            {
+                Console.WriteLine("NAO NEGATIVO");
+            }
+            else
+            {
+                Console.WriteLine("NEGATIVO");
+            }
+        }
+    }
+}

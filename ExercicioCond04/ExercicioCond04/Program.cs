ffb1b0611cb335b311f7502a3d59fc9071cfa87f
Fernando Mendes de Menezes <fmmenezes@hotmail.com>
2019-04-16 21:18:02 -0300

    Criação do Programa

---
+++ ExercicioCond04/ExercicioCond04/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExercicioCond04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

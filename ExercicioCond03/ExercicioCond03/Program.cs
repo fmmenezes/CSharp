b1b3926861cde62c4459fd9bbd171e7a079860e6
Fernando Mendes de Menezes <fmmenezes@hotmail.com>
2019-04-16 21:07:09 -0300

    Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao Multiplos", indicando se os valores lidos são múltiplos entre si. Atenção: os números devem poder ser digitados em ordem crescente ou decrescente

---
+++ ExercicioCond03/ExercicioCond03/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExercicioCond03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero:");
+            int A = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite outro numero:");
+            int B = int.Parse(Console.ReadLine());
+
+            if (A % B == 0 || B % A == 0) {
+                Console.WriteLine("Sao Multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Nao sao Multiplos");
+            }
+        }
+    }
+}

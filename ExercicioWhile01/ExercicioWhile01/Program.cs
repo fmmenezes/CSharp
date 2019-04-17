6bc93f120ffd378806d797a3707424c4bd08fe94
Fernando Mendes de Menezes <fmmenezes@hotmail.com>
2019-04-17 20:56:38 -0300

    Escreva um programa que repita a leitura de uma senha até que ela seja válida. Para cada leitura de senha incorreta informada, escrever a mensagem "Senha Invalida". Quando a senha for informada corretamente deve ser impressa a mensagem "Acesso Permitido" e o algoritmo encerrado. Considere que a senha correta é o valor 2002.

---
+++ ExercicioWhile01/ExercicioWhile01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExercicioWhile01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a Senha: ");
+            int senha = 0;
+            senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002)
+            {
+                Console.WriteLine("Senha Invalida");
+                Console.WriteLine("Digite a Senha: ");
+                senha = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Acesso Permitido");
+        }
+    }
+}

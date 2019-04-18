d660634495ac8288e664c6a7ee36452ca90a3c5f
Fernando Mendes de Menezes <fmmenezes@hotmail.com>
2019-04-17 22:08:17 -0300

    Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o X, se for o caso.

---
+++ ExercicioFor01/ExercicioFor01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ExercicioFor01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor: ");
+            int x = int.Parse(Console.ReadLine());
+
+            for(int a = 1; a <= x; a = a + 2)
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}

477a4c3fe1a69d877d0b39d109a33595ca8dd671
Fernando Mendes de Menezes <fmmenezes@hotmail.com>
2019-04-20 09:54:01 -0300

    Criação do Programa. Preço Total do Produto em Estoque

---
+++ A42Exercicio01/A42Exercicio01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace A42Exercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+
+            Console.WriteLine("Entre com os dados do produto:");
+
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine());
+
+            Console.Write("Quantidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p.Nome + ", $ " + p.Preco.ToString("F2") + ", " + p.Quantidade + " unidades, Total: $ " + p.ValorTotalEmEstoque().ToString("F2"));
+
+        }
+    }
+}

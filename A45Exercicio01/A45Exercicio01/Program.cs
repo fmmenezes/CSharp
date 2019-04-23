04d3b6755398ad256c14b6437f16739ce40baeda
Fernando Mendes de Menezes <fmmenezes@hotmail.com>
2019-04-22 22:50:00 -0300

    Fazer um programa para ler os valores da largura e altura de um retângulo. Em seguida, mostrar na tela o valor de sua área, perímetro e diagonal.

---
+++ A45Exercicio01/A45Exercicio01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace A45Exercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com a largura e altura do retangulo:");
+            //double Largura = double.Parse(Console.ReadLine());
+            //double Altura = double.Parse(Console.ReadLine());
+
+            Retangulo r = new Retangulo();
+
+            r.Largura = double.Parse(Console.ReadLine());
+            r.Altura = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("AREA = " + r.Area().ToString("F2"));
+            Console.WriteLine("PERIMETRO = " + r.Perimetro().ToString("F2"));
+            Console.WriteLine("DIAGONAL = " + r.Diagonal().ToString("F2"));
+        }
+
+        
+    }
+}

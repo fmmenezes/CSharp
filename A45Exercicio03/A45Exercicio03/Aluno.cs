b188783eddddd96ebbf1aef20522096dec591521
Fernando Mendes de Menezes <fmmenezes@hotmail.com>
2019-04-24 21:32:25 -0300

    Fazer um programa para ler o nome de um aluno e as três notas que ele obteve nos três trimestres do ano (primeiro trimestre vale 30 e o segundo e terceiro valem 35 cada). Ao final, mostrar qual a nota final do aluno no ano. Dizer também se o aluno está APROVADO ou REPROVADO e, em caso negativo, quantos pontos faltam para o aluno obter o mínimo para ser aprovado (que é 60 pontos). Você deve criar uma classe Aluno para resolver este problema.

---
+++ A45Exercicio03/A45Exercicio03/Aluno.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace A45Exercicio03
+{
+    class Aluno
+    {
+        public string Nome;
+        public double NotaT1, NotaT2, NotaT3;
+
+        public double SomaNota()
+        {
+            return NotaT1 + NotaT2 + NotaT3;
+        }
+
+        public double PontosFaltantes()
+        {
+            return 60 - SomaNota();
+        }
+
+    }
+}
---
+++ A45Exercicio03/A45Exercicio03/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace A45Exercicio03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+
+            Console.Write("Nome do aluno: ");
+            aluno.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as tres notas do aluno: ");
+            aluno.NotaT1 = double.Parse(Console.ReadLine());
+            aluno.NotaT2 = double.Parse(Console.ReadLine());
+            aluno.NotaT3 = double.Parse(Console.ReadLine());
+            double NotaF = aluno.SomaNota();
+            Console.WriteLine("NOTA FINAL = " + NotaF.ToString("F2"));
+
+            if (NotaF >= 60)
+            {
+                Console.WriteLine("APROVADO ");
+            }
+            else
+            {
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("FALTARAM " + aluno.PontosFaltantes().ToString("F2") + " PONTOS");
+            }
+
+
+            
+
+        }
+    }
+}

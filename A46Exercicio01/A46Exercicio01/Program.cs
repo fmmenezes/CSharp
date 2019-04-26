502ce2d2605a1650f68488b5fe0bd870bf41bb40
Fernando Mendes de Menezes <fmmenezes@hotmail.com>
2019-04-25 22:41:16 -0300

    Calculo de circunferencia e volume

---
+++ A46Exercicio01/A46Exercicio01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace A46Exercicio01
+{
+    class Program
+    {
+        static double Pi = 3.14;
+
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Circunferencia(raio);
+            Console.WriteLine("Circunferencia: " + circ.ToString("F2"), CultureInfo.InvariantCulture);
+
+            double vol = Volume(raio);
+            Console.WriteLine("Volume: " + vol.ToString("F2"),CultureInfo.InvariantCulture );
+            Console.WriteLine("Valor de PI: " + Pi);
+        }
+
+        static double Circunferencia(double r)
+        {
+            return 2 * Pi * r;
+        }
+
+        static double Volume(double r)
+        {
+            return 4.0 / 3.0 * Pi * r * r * r;
+        }
+    }
+}

be7929b26efad78b06ca06e89e8fa714c56aa08e
Fernando Mendes de Menezes <fmmenezes@hotmail.com>
2019-04-26 23:03:27 -0300

    Conversao de Dolar para Real nais IOF

---
+++ A48Exercicio01/A48Exercicio01/ConversorDeMoeda.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace A48Exercicio01
+{
+    class ConversorDeMoeda
+    {
+        static double iof = 6.0;
+
+        public static double DolarParaReal(double cotacao, double valor)
+        {
+            return cotacao * valor;
+        }
+
+        public static double AdicionaIOF(double valor)
+        {
+            return valor + (valor * 6 / 100);
+        }
+    }
+}
---
+++ A48Exercicio01/A48Exercicio01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace A48Exercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual e a cotacao do dolar? ");
+            double cot = double.Parse(Console.ReadLine());
+            Console.Write("Quantos dolares voce vai comprar? ");
+            double vlr = double.Parse(Console.ReadLine());
+            double total = ConversorDeMoeda.DolarParaReal(cot, vlr);
+            double totalIOF = ConversorDeMoeda.AdicionaIOF(total);
+            Console.WriteLine("Valor a ser pago em reais = " + totalIOF.ToString("F2"));
+        }
+    }
+}

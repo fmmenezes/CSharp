27a870900fc09cf81619217adddf1912b1d17c21
Fernando Mendes de Menezes <fmmenezes@hotmail.com>
2019-05-14 22:53:22 -0300

    Exercicio de Fixacao 60

---
+++ ExercicioFixacao60/ExercicioFixacao60/ContaBancaria.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioFixacao60
+{
+    class ContaBancaria
+    {
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        public ContaBancaria(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+        }
+
+        public ContaBancaria(int numero, string titular, double saldo) : this(numero, titular)
+        {
+            Saldo = saldo;
+        }
+
+        public override string ToString()
+        {
+            return "Conta " + Numero + ", Titular: " + Titular + ", Saldo: $ " + Saldo;
+        }
+    }
+}
---
+++ ExercicioFixacao60/ExercicioFixacao60/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExercicioFixacao60
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre o numero da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.WriteLine("Havera deposito inicial (s/n)? ");
+            char resposta = char.Parse(Console.ReadLine());
+
+            if(resposta == 's' || resposta == 'S')
+            {
+                Console.WriteLine("Entre com o valor do deposito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine());
+            }
+        }
+    }
+}

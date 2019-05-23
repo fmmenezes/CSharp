cc1f0b87bee0ff26bad32191a0a32126a33ca32e
Fernando Mendes de Menezes <fmmenezes@hotmail.com>
2019-05-22 22:24:54 -0300

    Exemplo Vetores

---
+++ A69Vetores/A69Vetores/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace A69Vetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double[] vect = new double[n];
+
+            double sum = 0.0;
+
+            for (int i = 0; i < n; i++)
+            {
+                vect[i] = double.Parse(Console.ReadLine());
+
+                sum += vect[i];
+
+            }
+
+            double avg = sum / n;
+
+            Console.WriteLine("AVERAGE HEIGHT = " + avg.ToString("F2"));
+
+            
+        }
+    }
+}

017b86092f7e33d028874633fcdb154c9690f0af
Fernando Mendes de Menezes <fmmenezes@hotmail.com>
2019-05-25 16:01:29 -0300

    Exemplo de vetor com objeto

---
+++ A70Vetores2/A70Vetores2/Produto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace A70Vetores2
+{
+    class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+    }
+}
---
+++ A70Vetores2/A70Vetores2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace A70Vetores2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Quantidade de Produtos
+            Console.Write("Informe a quantidade de produtos: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Produto[] vetor = new Produto[quantidade];
+            double soma = 0;
+
+            for (int i = 0; i < quantidade; i++) {
+
+                Console.Write("informe a descrição do produto " + i + ": ");
+                string descricao = Console.ReadLine();
+                Console.Write("Infome o preço do produto " + i + ": ");
+                double preco = double.Parse(Console.ReadLine());
+
+                vetor[i] = new Produto { Nome = descricao, Preco = preco };
+
+                Console.WriteLine("Inserido no vetor o produto: " + vetor[i].Nome + " com valor: " + vetor[i].Preco);
+
+                soma += vetor[i].Preco;
+
+            }
+
+            double media = soma / quantidade;
+
+            Console.WriteLine("Preço médio = " + media );
+
+
+        }
+    }
+}

5bda0d164fb4d1974229ae2ec3ea389f43a2861b
Fernando Mendes de Menezes <fmmenezes@hotmail.com>
2019-05-28 20:05:55 -0300

    Exercicio de Fixacao com Vetores

---
+++ A71Exercicio/A71Exercicio/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace A71Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos quartos serão alugados? ");
+            int quantidade = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Aluguel[] vetor = new Aluguel[quantidade];
+
+            for (int i = 0; i < quantidade; i++)
+            {
+                int numero = i + 1;
+                Console.WriteLine("Aluguel #" + numero +":");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+
+                vetor[i] = new Aluguel { Nome = nome, Email = email, Quarto = quarto };
+            }
+
+            Console.WriteLine("Quartos ocupados: ");
+
+            for (int i = 0; i < quantidade; i++)
+            {
+                Console.WriteLine(vetor[i].Quarto + ": " + vetor[i].Nome + ", " + vetor[i].Email);
+            }
+
+        }
+    }
+}

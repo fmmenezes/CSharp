fcabed10923e84a5fc91c60a127343a3b83878bd
Fernando Mendes de Menezes <fmmenezes@hotmail.com>
2019-05-28 22:16:18 -0300

    Exemplo com Sobrecarga

---
+++ A72Exemplo01/A72Exemplo01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace A72Exemplo01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = Somador.Soma(double.Parse(Console.ReadLine()), double.Parse(Console.ReadLine()), double.Parse(Console.ReadLine()));
+                    
+            
+            Console.WriteLine(x);
+        }
+    }
+}
---
+++ A72Exemplo01/A72Exemplo01/Somador.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace A72Exemplo01
+{
+    class Somador
+    {
+        public static double Soma(double num1 , double num2)
+        {
+            return num1 + num2;
+        }
+
+        public static double Soma(double num1, double num2, double num3)
+        {
+            return num1 + num2 + num3;
+        }
+    }
+}
